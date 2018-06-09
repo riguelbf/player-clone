830a22cf810f3e18e12c547b57903381c8b7c1a3
riguelbf <riguel@dkosoftware.com.br>
2018-06-09 15:41:49 -0300

    create menu of sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 
-import { Container, NewPlaylist } from './styles';
+import { Container, NewPlaylist, Nav } from './styles';
 
 import AddPlaylistIcon from '../../assets/images/add_playlist.svg';
 
 const SideBar = () => (
   <Container>
-    <div />
+    <div>
+      <Nav main>
+        <li>
+          <a href="">Navegar</a>
+        </li>
+        <li>
+          <a href="">Rádio</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>Your library</span>
+        </li>
+        <li>
+          <a href="">Your daily list </a>
+        </li>
+        <li>
+          <a href="">Played recently</a>
+        </li>
+        <li>
+          <a href="">Musics</a>
+        </li>
+        <li>
+          <a href="">Albuns</a>
+        </li>
+        <li>
+          <a href="">Music stations</a>
+        </li>
+        <li>
+          <a href="">Local files</a>
+        </li>
+        <li>
+          <a href="">Videos</a>
+        </li>
+        <li>
+          <a href="">Podcasts</a>
+        </li>
+      </Nav>
+      <Nav>
+        <li>
+          <span>Best of musices</span>
+        </li>
+        <li>
+          <a href="">The best of the rock</a>
+        </li>
+      </Nav>
+    </div>
 
     <NewPlaylist>
       <img src={AddPlaylistIcon} alt="Add new playlist" />
